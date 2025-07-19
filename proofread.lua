5a431148c722e38449a51da2fb85b15594858dab
Roland Illig <roland.illig@gmx.de>
2025-07-19 14:29:29 +0200

    GCC: Optionen ohne umschließende Anführungszeichen

--- proofread.lua
+++ proofread.lua
@@ -38,22 +38,85 @@ function extract_c_format_percent(str)
   return percents
 end
 
+--- XXX: Ist strenggenommen redundant.
+--- XXX: Wie kann diese Info automatisch aus po.lua importiert werden?
+--- @shape PoMessage
+--- @field id number
+--- @field file string
+--- @field comments string[]
+--- @field msgid string
+--- @field msgstr string
+--- @field format table<string, boolean>
+--- @field gcc_internal_format boolean
+--- @field gfc_internal_format boolean
+--- @field c_format boolean
+--- @field no_c_format boolean
+
+--- @param msg PoMessage
+--- @param s string
+--- @return string
+local function gcc_internal_format_without_quoted_parts(msg, s)
+  if msg.gcc_internal_format then
+    return (s:gsub("%%<.-%%>", "X"))
+  end
+  return (s:gsub("».-«", "X"))
+end
+
+--- @param s string
+--- @param from string
+--- @param to string
+local function replace_once(s, from, to)
+  local replaced, n = s:gsub(from:gsub("[+%-]", "%%%1"), to)
+  if n == 1 then
+    return replaced
+  end
+  return s
+end
+
+--- @param msg PoMessage
+local function check_option_unquoted(msg)
+  if msg.msgid == ""
+    or msg.msgstr:find("^ ")
+    or msg.msgid:find("^%(Obsolete.*\t")
+    or msg.msgid == "%s%sGGC heuristics: --param ggc-min-expand=%d --param ggc-min-heapsize=%d\n"
+    or msg.msgid == "Abbreviation for \"-g -feliminate-unused-debug-symbols\"."
+    or msg.msgid == "Abbreviation for \"-g -fno-eliminate-unused-debug-symbols\"." then
+    return
+  end
+  local without_quotes = gcc_internal_format_without_quoted_parts(msg, msg.msgstr)
+  --- @param option string
+  for option in without_quotes:gmatch(" (%-[%-0-9A-Z_a-z]+=?[+%-0-9:A-Z_a-z]*)") do
+    if not option:match("^%-[0-9]+$")
+      and msg.msgid:find(option, 1, true)
+      and option ~= "--"
+      and option ~= "-INF" then
+      warn(msg, ("Option »%s«"):format(option))
+      if msg.gcc_internal_format then
+        msg.msgstr = replace_once(msg.msgstr, option, "%%<" .. option .. "%%>")
+      else
+        msg.msgstr = replace_once(msg.msgstr, option, "»" .. option .. "«")
+      end
+    end
+  end
+end
+
 function proofread(msg, msgid, msgstr)
   if msgstr == "" or msgstr == msgid or msg.fuzzy then
     return
   end
-  -- TODO: Options inside quotes are fine.
-  for option in msgid:gmatch(" (%-[A-Za-z]+[=][A-Za-z0-9%-]+)") do
-    warn(msg, ("Option »%s«"):format(option))
-  end
+  check_option_unquoted(msg)
   -- TODO: %<...%> ohne exakte Übersetzung
+  -- TODO: option -> Option/Schalter
+  -- TODO: stattdessen -> verwenden Sie stattdessen ...
+  -- TODO: Verwendung mit Schalter -> mit der
   if msgid:find("^[Uu]sage:") and not msgstr:find("^Aufruf:") then
     warn(msg, "»usage« sollte mit »Aufruf« übersetzt werden.", "^%a+")
   end
   if msgid:find("^%s") and not msgstr:find("^%s") then
     warn(msg, "Da im englischen Text Leerzeichen am Zeilenanfang sind, sollte das im deutschen Text auch so sein.")
   end
-  if msgid:find("%s$") and not msgstr:find("%s$") then
+  if msgid:find("%s$") and not msgstr:find("%s$")
+    and msgid ~= "Negation of unsigned expression at %L not permitted " then
     warn(msg, "Da im englischen Text Leerzeichen am Zeilenende sind, sollte das im deutschen Text auch so sein.")
   end
   if msgid:find("link") and not msgstr:find("[Bb][iu]nd") then
@@ -73,9 +136,7 @@ function proofread(msg, msgid, msgstr)
       "Im deutschen Text sollten keine \"geraden\", " ..
       "sondern „diese“ oder »jene« Anführungszeichen benutzt werden.",
       "\\\"")
-    local corrected = autocorrectQuotes(msgstr)
-    if false and corrected ~= msgstr and promptCorrect() then
-    end
+    msg.msgstr = msgstr:gsub("\\\"([%a%%][^\"]*%a)\\\"", "»%1«")
   end
   if msgstr:find("%f[%l]the%f[%L]") and msgid ~= "" then
     warn(msg, "»the« gefunden – möglicherweise nicht vollständig übersetzt.")
@@ -102,20 +163,6 @@ function color(n)
   return haveColor and string.char(0x1B) .. "[" .. n .. "m" or ""
 end
 
-function autocorrectQuotes(s)
-  return s:gsub("\\\"([%a%%][^\"]*%a)\\\"", "»%1«")
-end
-
-function promptCorrect()
-  io.write("Korrigieren [j]? ")
-  io.flush()
-  local answer = io.read("*line")
-  if answer == "" then
-    io.write("GEHT NOCH NICHT\n")
-    return true
-  end
-end
-
 function highlight(s, n)
   return color(n) .. s .. color(0)
 end
@@ -125,6 +172,8 @@ function markred(s, redpattern)
   return s:gsub(redpattern, color(31) .. color(4) .. "%1" .. color(0))
 end
 
+--- @param msg PoMessage
+--- @param redpattern string | nil
 function warn(msg, warning, redpattern)
   local function fmtmsg(s)
     return "\"" .. s:gsub("\\n(.)", "\\n\"\n\"%1") .. "\""
@@ -148,12 +197,13 @@ end
 function proofread_file(fname)
   local file = require("proofread/po").File:new()
   file:parse(fname)
+  --- @param msg PoMessage
   for _, msg in ipairs(file.messages) do
     if msg.msgstr ~= nil then
       proofread(msg, msg.msgid, msg.msgstr)
     end
   end
-  --file:write(fname .. "c")
+  file:write(fname .. "c")
 end
 
 function main(arg)
